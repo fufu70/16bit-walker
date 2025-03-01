e8570796ae7ee7c8433e75bdd0c07aa44857aa21
Linette Melgara <linettemelgara@Linettes-MacBook-Pro.local>
2025-03-01 17:46:14 -0500

    pushed build update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-  	allowedHosts: ['9tige9-ip-71-76-109-82.tunnelmole.net']
-  },
+  root: "./",
+  publicDir: "./public"
 })
